189b20003ba7eb3fe50bb13b70a58e2dae712f50
Manuel <manu@batkiwi.com>
2013-03-11 22:53:37 +0100

    Added 2 examples

--- foxyhtml.py
+++ foxyhtml.py
@@ -272,22 +272,45 @@ class FoxyHtml(list):
         return self[0].id()
 
 
-def main(html):
+def main():
     import urllib2
     from pprint import pprint
     html = urllib2.urlopen("http://www.thetimes.co.uk").read()
     parsed = FoxyHtml(html)
 
+    print
+    print "Example1"
     print "Way 1:"
     results = parsed.search(cls="nib-inner")
     for result in results:
         pprint(
             [result.joinedtexts() for result in result.search(tagname="li")])
 
+    print
     print "Way 2:"
     pprint(
         [result.joinedtexts() for result in parsed.foxycss(".nib-inner li")])
 
+    print
+    print "Example2"
+    pprint([dict(zip(["title", "body"], fields))
+            for bricks in parsed.search(cls="brick-mpu")
+            for brick in bricks.search(cls="brick-sixth")
+            for fields in [brick.search(cls="f-hc").joinedtexts()]
+            if all(fields)
+            ])
+
+    print
+    print "Example3"
+    pprint([{
+            'title': titles[0],
+            'subtitile': titles[1],
+            'body': brick.search(tagname="p").joinedtexts()
+            }
+            for bricks in parsed.search(cls="brick-arts")
+            for brick in bricks.search(cls="brick-sixth")
+            for titles in brick.search(cls="f-hc").texts()
+            ])
 
 if __name__ == "__main__":
-    main("")
+    main()
